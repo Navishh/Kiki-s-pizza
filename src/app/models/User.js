91a1f8d72ca067b92d047ffc96b4caeb11189285
Navishh <navishkanuwan11@gmail.com>
2024-06-29 11:27:06 +0530

    chore: Error handliing

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,17 +1,22 @@
 import { Schema, model, models } from "mongoose";
 import Error from "next/error";
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: {
-    type: String,
-    required: true,
-    validate: (pass) => {
-      if (!pass?.length || pass.length < 5) {
-        new Error(" Password must be at least 5 characters long");
-      }
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate: (pass) => {
+        if (!pass?.length || pass.length < 5) {
+          new Error('Password must be at least 5 characters long');
+          return false;
+        }
+      },
     },
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export const User = models?.User || model("User", UserSchema);
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 import RightArrow from "../icons/rightarrow";
 
